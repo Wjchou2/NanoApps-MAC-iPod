87a6f10fbabcfd8360e0f71b183576e5b524568c
WilliamChou123 <wchou28@shschools.org>
2026-06-08 13:37:37 -0700

    test

--- apps/my_app/my_app.c
+++ apps/my_app/my_app.c
@@ -2,9 +2,15 @@
 #include "lvgl.h"
 
 static void changed(lv_event_t *e) {
+    lv_obj_t *label = lv_event_get_user_data(e);
     lv_obj_t *slider = lv_event_get_target(e);
     int value = lv_slider_get_value(slider);
     hb_brightness_set(value);
+    char text[16];
+    snprintf(text, sizeof(text), "%d", value);
+    lv_label_set_text(label, text);
+
+
 }
 
 HB_APP_ENTRY(payload_entry) {
@@ -16,5 +22,12 @@ HB_APP_ENTRY(payload_entry) {
     lv_slider_set_range(slider, 0, 100);
     lv_slider_set_value(slider, 0, LV_ANIM_OFF);
 
-    lv_obj_add_event_cb(slider, changed, LV_EVENT_VALUE_CHANGED, NULL);
+    lv_slider_set_range(slider, 0, 100);
+
+    lv_obj_t *label = lv_label_create(lv_screen_active());
+
+    lv_obj_set_width(label, 180);
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 20);
+
+    lv_obj_add_event_cb(slider, changed, LV_EVENT_VALUE_CHANGED, label);
 }
